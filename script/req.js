cb643378e458863dd12b85202e8a9758f42b2da8
unknown <patnj18@gmail.com>
2021-03-02 01:14:43 +0700

    update Web

--- script/req.js
+++ script/req.js
@@ -1,10 +1,19 @@
 
+//Test
 const Ramber = {
     Host : "Test",
     Text1 : "Yeah Our project is working.",
     Text2 : "Thank you for Test Our project.",
     Text3 : "We want to say thank you to all of yours."
   };
+
+//Example
+const Patnj18 ={
+    Host : "Pat 6/15",
+    Text1 : "",
+    Text2 : "",
+    Text3 : ""
+};
 
 button5.onpointerdown = function (){
 
@@ -24,11 +33,11 @@ button5.onpointerdown = function (){
 
     //Use this condition to show pop up
     if (count >2){
-        alert(Json["Text3"]);
+        alert(Ramber["Text3"]);
     }else if (count >0){
-        alert(Json["Text2"]);
+        alert(Ramber["Text2"]);
     }else{
-        alert(Json["Text1"]);
+        alert(Ramber["Text1"]);
     }
     
 };
