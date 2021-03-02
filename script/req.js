75dbfd32fa7aeeaecc90bd81dfb17c83d3d471a1
unknown <patnj18@gmail.com>
2021-03-02 11:56:48 +0700

    Json request testing Script

--- script/req.js
+++ script/req.js
@@ -16,6 +16,19 @@ const Patnj18 ={
 };
 
 button5.onpointerdown = function (){
+    //get Json file
+    let requestURL = "https://raw.githubusercontent.com/fifa0095/fefa/main/Test.json";
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+        const JsonFile = request.response;
+        const Text1 = JsonFile["Text1"];
+        const Text2 = JsonFile["Text2"];
+        const Text3 = JsonFile["Text3"];
+    }
 
     // Created loop through 2 Array
     for(let i=0; i<Answer.length; i++) {
@@ -33,11 +46,11 @@ button5.onpointerdown = function (){
 
     //Use this condition to show pop up
     if (count >2){
-        alert(Ramber["Text3"]);
+        alert(Text3);
     }else if (count >0){
-        alert(Ramber["Text2"]);
+        alert(Text2);
     }else{
-        alert(Ramber["Text1"]);
+        alert(Text1);
     }
     
 };
